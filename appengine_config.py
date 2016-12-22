d2daf628b59f07c291c4cfd23e86d176d7eda610
Alex Ford <fordas@uw.edu>
2016-12-22 12:10:32 -0800

    Convert to Flask from webapp2.
    Convert core app to Flask. Vendorize flask & dependencies and move
    vendor to `lib`. Move templates. Add redirect handlers for index and
    composite.

--- appengine_config.py
+++ appengine_config.py
@@ -3,4 +3,4 @@ import os
 from google.appengine.ext import vendor
 
 # Add any libraries install in the "external" folder.
-vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'external'))
+vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import os
 import io
 
-import webapp2
-import jinja2
+from flask import Flask
+app = Flask(__name__)
 
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape'],
-    autoescape=True)
+from flask import abort, redirect, url_for, render_template, Response, request
 
 import requests
 import requests_toolbelt.adapters.appengine
@@ -15,60 +12,43 @@ requests_toolbelt.adapters.appengine.monkeypatch()
 
 from tilesource import clip_image_alpha, overlay_image, source_images, parse_tilespec
 
-class DirectRender(webapp2.RequestHandler):
+@app.route("/")
+@app.route("/composite/")
+def composite_redirect():
+    return redirect(url_for("composite", tilespec="im_fs-32_ct-64"))
 
-    def get(self, tiletype, z, x, y):
-        tile_url = sources[tiletype].format(z=z, x=x, y=y)
-        r = requests.get(tile_url)
-        r.raise_for_status()
+@app.route("/composite/<tilespec>/")
+def composite(tilespec):
+    # Parse to assert that tilespec valid
+    parse_tilespec(tilespec)
+    return render_template('composite/index.html', tilespec=tilespec)
 
-        self.response.headers['Content-Type'] = r.headers["Content-Type"]
-        self.response.write(r.content)
+@app.route("/composite/<tilespec>/tilejson")
+def composite_tilejson(tilespec):
+    parse_tilespec(tilespec)
 
-class CompositeTile(webapp2.RequestHandler):
+    return Response(
+        render_template("composite/tilejson.json", tilespec=tilespec),
+        content_type='application/json; charset=utf-8'
+    )
 
-    def get(self, tilespec):
-        params = {
-            p : self.request.get(p)
-            for p in ("z", "x", "y")
-        }
+@app.route("/composite/<tilespec>/tile")
+def composite_tile(tilespec):
+    params = {
+        p : request.args.get(p)
+        for p in ("z", "x", "y")
+    }
 
-        tilespec = parse_tilespec(tilespec)
+    tilespec = parse_tilespec(tilespec)
 
-        tile_images = source_images(keys = [l for l, a in tilespec], **params)
-            
-        composite = overlay_image(*[
-            clip_image_alpha(tile_images[layer], alpha)
-            for layer, alpha in tilespec
-        ])
+    tile_images = source_images(keys = [l for l, a in tilespec], **params)
+        
+    composite = overlay_image(*[
+        clip_image_alpha(tile_images[layer], alpha)
+        for layer, alpha in tilespec
+    ])
 
-        b = io.BytesIO()
-        composite.save(b, format='png')
+    b = io.BytesIO()
+    composite.save(b, format='png')
 
-        self.response.headers["Content-Type"] = "image/png"
-        self.response.write(b.getvalue())
-
-class CompositeIndex(webapp2.RequestHandler):
-    def get(self, tilespec):
-        # Parse to assert that tilespec valid
-        parse_tilespec(tilespec)
-        template_values = dict(tilespec = tilespec)
-        template = JINJA_ENVIRONMENT.get_template('composite/index.html')
-        self.response.write(template.render(template_values))
-
-class CompositeTilejson(webapp2.RequestHandler):
-    def get(self, tilespec):
-        # Parse to assert that tilespec valid
-        parse_tilespec(tilespec)
-        template_values = dict(tilespec = tilespec)
-        template = JINJA_ENVIRONMENT.get_template('composite/tilejson.json')
-        self.response.headers["Content-Type"] = "application/json"
-        self.response.write(template.render(template_values))
-
-app = webapp2.WSGIApplication([
-    webapp2.Route(r'/direct/<tiletype:\w+>/<z:\d+>/<x:\d+>/<y:\d+>.png', handler=DirectRender),
-    webapp2.Route(r'/composite/<tilespec:[^/]+>/tile', handler=CompositeTile),
-    webapp2.Route(r'/composite/<tilespec:[^/]+>/tilejson', handler=CompositeTilejson),
-    webapp2.Route(r'/composite/<tilespec:[^/]+>/', handler=CompositeIndex),
-    webapp2.Route(r'/composite/<tilespec:[^/]+>', handler=CompositeIndex),
-], debug=True)
+    return Response(b.getvalue(), content_type='img/png')
